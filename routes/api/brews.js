e54a74170b603f24b63fe9e288c7041340bb69f0
Jack Ceballos <jackrceballos@gmail.com>
2022-01-14 18:50:06 -0500

    Minor changes added

--- routes/api/brews.js
+++ routes/api/brews.js
@@ -3,18 +3,18 @@ const router = require('express').Router();
 
 // Read
 // localhost:3001/api/brews/
-router.get('/')
+router.get('/');
 
 // Create
 // localhost:3001/api/brews/new
-router.post('/new')
+router.post('/new');
 
 // Update
 // localhost:3001/api/brews/update/:id
-router.put('/update/:id')
+router.put('/update/:id');
 
 // Delete
 // localhost:3001/api/brews/remove/:id
-router.delete('/remove/:id')
+router.delete('/remove/:id');
 
 module.exports = router;
