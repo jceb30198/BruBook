11f801545e5163c242088779fa70b3d17624cfbe
Jack Ceballos <jackrceballos@gmail.com>
2022-01-16 22:37:43 -0500

    Finished the MVP for the React Skeleton. Next is to add styling and API communication.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,19 +3,36 @@ function App() {
     <div>
       <form className="form">
         <div className="form-group">
-          <label htmlFor="original-gravity">Original Gravity (OG)</label>
+          <label htmlFor="beer-name">Name of Beer:</label>
+          <input 
+          id="beer-name" 
+          name="Beer Name" 
+          type="text" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="original-gravity">Original Gravity (OG):</label>
           <input
           id="original-gravity"
+          name="Original Gravity"
           type="number" />
         </div>
         <div className="form-group">
-          <label htmlFor="final-gravity">Final Gravity (FG)</label>
+          <label htmlFor="final-gravity">Final Gravity (FG):</label>
           <input
           id="final-gravity"
+          name="Final Gravity"
           type="number" />
         </div>
         <input type="submit" value="Calculate ABV"/>
       </form>
+      <div className="display-abv">
+        <h3>ABV GOES HERE</h3>
+      </div>
+      <div className="display-brews">
+        <ul>
+          <li>PREVIOUS BEER DATA GOES HERE</li>
+        </ul>
+      </div>
     </div>
   )
 }
