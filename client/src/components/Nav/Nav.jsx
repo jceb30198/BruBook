dc8cf4597f2402b82b316cc5aa8d3ad5924943a8
Jack Ceballos <jackrceballos@gmail.com>
2022-01-24 20:23:29 -0500

    Working on the Nav component since I plan on adding more pages to this application.

---
+++ client/src/components/Nav/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './style.css';
+
+export default function Nav() {
+  return (
+    <header>
+      <div className='container'>
+        <nav id='main-nav'>
+          <h1>hello</h1>
+          <ul>
+            <li>Home</li>
+            <li>IBU</li>
+            <li>SRM</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+} 
