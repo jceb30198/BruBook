e2b28632fd1f2f0aadb015f9afc88a3c6a667859
Jack Ceballos <jackrceballos@gmail.com>
2022-01-24 20:47:49 -0500

    Added Font-Awesome npm package for the beer icon. Styled the Nav component to look a little nicer. Going to push these changes because I do not have much more time for anything else today.

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBeer } from '@fortawesome/free-solid-svg-icons'
 import './style.css';
 
 export default function Nav() {
   return (
     <header>
       <div className='container'>
-        <nav id='main-nav'>
-          <h1>hello</h1>
+        <nav id='flex-nav'>
+          <FontAwesomeIcon icon={ faBeer } size='2x' />
           <ul>
             <li>Home</li>
             <li>IBU</li>
