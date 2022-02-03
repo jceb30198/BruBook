5e46ff93e94ea64e409b761a29cbf98043735681
Jack Ceballos <jackrceballos@gmail.com>
2022-02-03 12:26:02 -0500

    Got the active link working but I am not completely sure how.

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBeer } from '@fortawesome/free-solid-svg-icons'
 import './style.css';
 
 export default function Nav() {
-  let params = useParams();
-
-  console.log(params)
   return (
     <header>
       <div className='nav-container'>
@@ -17,8 +14,8 @@ export default function Nav() {
           <h2>BruBook</h2>
           </div>
           <ul>
-            <li><Link className='link' to='/'>ABV Calc</Link></li>
-            <li><Link className='link' to='/brewd'>BrewD</Link></li>
+            <li><NavLink className='link' to='/'>ABV Calc</NavLink></li>
+            <li><NavLink className='link' to='/brewd'>BrewD</NavLink></li>
           </ul>
         </nav>
       </div>
