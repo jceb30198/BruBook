82fad286464ba4436440fecd8ae0ba143a02cfe5
Jack Ceballos <jackrceballos@gmail.com>
2022-02-03 21:03:06 -0500

    Setting up the skeleton for the brewd page.

--- client/src/pages/Brewd.jsx
+++ client/src/pages/Brewd.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { API } from '../utils/API'
 
 export default function Brewd() {
+  const [brews, setBrews] = useState([]);
+
+  console.log(brews);
+  useEffect(() => {
+    API.getBrews()
+      .then(data => setBrews(data))
+      .catch(err => console.error(err));
+  }, [])
+
   return (
-   <main>
-     <h1>Hello World this is My Project</h1>
-   </main>
+    <main>
+      <div className="brewd-container">
+        
+      </div>
+    </main>
   )
 }
