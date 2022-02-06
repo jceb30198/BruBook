3eca7a490c5c6dba29fa97ed6a1e3ec6fa75faa4
Jack Ceballos <jackrceballos@gmail.com>
2022-02-05 19:15:41 -0500

    Forgot the delete functionality on the BrewD page. Now this branch is done and I will work on media queries.

--- client/src/pages/Brewd.jsx
+++ client/src/pages/Brewd.jsx
@@ -4,11 +4,21 @@ import { API } from '../utils/API'
 export default function Brewd() {
   const [brews, setBrews] = useState([]);
 
+  // Get All Brews
   useEffect(() => {
     API.getBrews()
       .then(data => setBrews(data))
       .catch(err => console.error(err));
   }, [])
+
+  // Handle the Brew Delete
+  const handoleDelete = (id) => {
+    // Deletes from DB
+    API.deleteBrew(id);
+
+    // Deletes from State
+    setBrews((brews) => brews.filter(brew => brew._id !== id));
+  }
 
   return (
     <main>
@@ -20,7 +30,9 @@ export default function Brewd() {
               <p>Original Gravity: {brew.originalGrav}</p>
               <p>Final Gravity: {brew.finalGrav}</p>
               <p>ABV: {brew.abv}%</p>
-              <button className="btn delete">X</button>
+              <button 
+              className="btn delete"
+              onClick={() => handoleDelete(brew._id)}>X</button>
             </div>
           )
         })}
